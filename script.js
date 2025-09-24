bf5b59b97dbda52b673b580f625d9af7a5ab69b7
saprameya <sheela_aprameya@yahoo.com>
2025-09-24 10:51:01 -0400

    minor

--- script.js
+++ script.js
@@ -369,6 +369,7 @@ $().ready(() => {
 });
 
 function showAlert(message) {
+  
   $("#dialog").dialog({
     autoOpen: false,
     show: {
@@ -380,8 +381,8 @@ function showAlert(message) {
       duration: 1000,
     },
     position: {
-      my: "center ",
-      at: "center ",
+      my: "center",
+      at: "center",
       of: $(".puzzle"),
     },
     modal: true,
